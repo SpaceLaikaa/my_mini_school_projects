b4d8e0df5b80ee32981c8c440f4cca954a513285
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-10-24 22:55:43 +0300

    Lab assignments from school and self-made mini projects

---
+++ bymyself/MultiplicationTest.java
@@ -0,0 +1,17 @@
+package bymyself;
+
+import java.util.Scanner;
+
+public class MultiplicationTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("You're about to multiply two numbers. Please enter the first number: ");
+        int a = input.nextInt();
+
+        System.out.println("Now enter the second number: ");
+        int b = input.nextInt();
+
+        System.out.print("Here is the result of your multiplication: " + (a * b));
+    }
+}
