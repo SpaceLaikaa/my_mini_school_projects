00e210e689da1e208ac75d5a78278eb96efadb3d
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-10-31 12:05:11 +0300

    Rename files & add new lab assignments

---
+++ lab5assignment0.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class lab5assignment1 {
+    public static int factorial(int a){
+
+        if (a == 0){
+            return 1;
+        }
+            int result = a * factorial(a-1);
+            return(result);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(factorial(n));
+    }
+}
