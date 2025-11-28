d06a468aa3974df6db3dffac277a177c4d30d61c
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-11-28 12:25:43 +0300

    Initial commit for lab 7 assignments

---
+++ L7Assignment0/Point.java
@@ -0,0 +1,13 @@
+package L7Assignment0;
+
+import java.util.Scanner;
+
+public class Point {
+    public int x;
+    public int y;
+
+    public Point() {
+        x = 10;
+        y = 12;
+    }
+}
---
+++ L7Assignment0/lab7assignment0.java
@@ -0,0 +1,10 @@
+package L7Assignment0;
+
+public class lab7assignment0 {
+    public void main(String[] args) {
+    Point p = new Point();
+    Circle c = new Circle();
+
+
+    }
+}
