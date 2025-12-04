fe997ab5ce859f31de14e0b72bae5af6b6426337
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-12-05 00:08:04 +0300

    Implement Task class with energy costs and integrate task selection logic with battery validation into the demo

--- lab_projects/lab8/L8Assignment1/FactoryDemoL8A1.java
+++ lab_projects/lab8/L8Assignment1/FactoryDemoL8A1.java
@@ -6,6 +6,10 @@ public class FactoryDemoL8A1 {
     public void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //Part C
+        Task Task1 = new Task("Clean",25);
+        Task Task2 = new Task("Wash", 35);
+
         //Part A
         Robot Robo1 = new Robot("Nagasaki-15", 100,"Active");
         Robot Robo2 = new Robot("Osaka-38", 100,"Active");
@@ -20,7 +24,28 @@ public class FactoryDemoL8A1 {
         System.out.println("\nHow much battery do you want to consume on Robot 1? ");
         int consumeBattery = sc.nextInt();
         int consumedBatteryR1 = Robo1.consumeBattery(consumeBattery);
-
         System.out.println("Robot 1's new battery is: " + consumedBatteryR1 + "%");
+
+        //Part C
+        System.out.println("Task 1: Clean (25% Consumation)\nTask 2: Wash (35% Consumation)");
+        int whichTask = sc.nextInt();
+
+        switch (whichTask){
+            case 1:{
+                if (Robo1.isBatteryEnough(Task1.getEnergyCost())){
+                    consumedBatteryR1 -= Task1.getEnergyCost();
+                    System.out.println("Cleaning process.... Done! New Battery: " + consumedBatteryR1);
+                    break;
+                }
+                else System.out.println("Not Enough Battery..."); break;
+            }
+            case 2:
+                if(Robo1.isBatteryEnough(Task2.getEnergyCost())){
+                    consumedBatteryR1 -= Task2.getEnergyCost();
+                    System.out.println("Washing process.... Done! New Battery: " + consumedBatteryR1);
+                    break;
+                }
+                else System.out.println("Not Enough Battery..."); break;
+        }
     }
 }
---
+++ lab_projects/lab8/L8Assignment1/Task.java
@@ -0,0 +1,18 @@
+package lab_projects.lab8.L8Assignment1;
+
+public class Task {
+    //part C
+    private String taskName;
+    private int energyCost;
+
+    public Task(String TN, int EC){
+        this.taskName = TN;
+        this.energyCost = EC;
+    }
+
+    public String getTaskName(){return taskName;}
+    public int getEnergyCost(){return energyCost;}
+
+    public void setTaskName(String taskName){this.taskName = taskName;}
+    public void setEnergyCost(int energyCost){this.energyCost = energyCost;}
+}
