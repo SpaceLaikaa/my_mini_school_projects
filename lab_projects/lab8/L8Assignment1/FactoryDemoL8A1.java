438b74bd7264f48c5cb1d6b5dfef35e423e4b7e0
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-12-04 21:58:11 +0300

    Initial commit for lab 8, continuing classes in java.

---
+++ lab_projects/lab8/L8Assignment1/FactoryDemoL8A1.java
@@ -0,0 +1,5 @@
+package lab_projects.lab8.L8Assignment1;
+
+public class FactoryDemoL8A1 {
+
+}
---
+++ lab_projects/lab8/L8Assignment1/Robot.java
@@ -0,0 +1,7 @@
+package lab_projects.lab8.L8Assignment1;
+
+public class Robot {
+    private String modelName;
+    private int batteryLevel;
+    private String status;
+}
