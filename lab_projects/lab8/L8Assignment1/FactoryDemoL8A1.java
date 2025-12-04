0dd648820d01fbd1d03b3a4bf5cbe611afd57da2
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-12-04 23:38:52 +0300

    Add battery consumption logic and user input for Part B and organised the code

--- lab_projects/lab8/L8Assignment1/FactoryDemoL8A1.java
+++ lab_projects/lab8/L8Assignment1/FactoryDemoL8A1.java
@@ -1,7 +1,12 @@
 package lab_projects.lab8.L8Assignment1;
 
+import java.util.Scanner;
+
 public class FactoryDemoL8A1 {
     public void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Part A
         Robot Robo1 = new Robot("Nagasaki-15", 100,"Active");
         Robot Robo2 = new Robot("Osaka-38", 100,"Active");
 
@@ -10,5 +15,12 @@ public class FactoryDemoL8A1 {
         System.out.println("");
         System.out.println("=====Robot 2=====");
         Robo2.displayRobotInfo();
+
+        //Part B
+        System.out.println("\nHow much battery do you want to consume on Robot 1? ");
+        int consumeBattery = sc.nextInt();
+        int consumedBatteryR1 = Robo1.consumeBattery(consumeBattery);
+
+        System.out.println("Robot 1's new battery is: " + consumedBatteryR1 + "%");
     }
 }
--- lab_projects/lab8/L8Assignment1/Robot.java
+++ lab_projects/lab8/L8Assignment1/Robot.java
@@ -7,22 +7,44 @@ public class Robot {
     private int batteryLevel;
     private String status;
 
-    public Robot(String MN, int BL, String status){
+    public Robot(String MN, int BL, String status) {
         this.modelName = MN;
         this.batteryLevel = BL;
         this.status = status;
     }
 
-    public void displayRobotInfo(){
-        System.out.println("Model Name: " + modelName +"\nBattery Level: " + batteryLevel + "%\nStatus: " + status);
+    //Part A
+    public void displayRobotInfo() {
+        System.out.println("Model Name: " + modelName + "\nBattery Level: " + batteryLevel + "%\nStatus: " + status);
     }
 
-    public String getModelName(){return modelName;}
-    public int getBatteryLevel(){return batteryLevel;}
-    public String getStatus(){return status;}
+    public String getModelName() {return modelName;}
 
-    public void setModelName(String modelName){this.modelName = modelName;}
-    public void setBatteryLevel(int batteryLevel){this.batteryLevel = batteryLevel;}
-    public void setStatus(String status){this.status = status;}
+    public int getBatteryLevel() {return batteryLevel;}
 
+    public String getStatus() {return status;}
+
+    public void setModelName(String modelName) {this.modelName = modelName;}
+
+    public void setBatteryLevel(int batteryLevel) {this.batteryLevel = batteryLevel;}
+
+    public void setStatus(String status) {this.status = status;}
+
+
+    //Part B
+    public boolean isBatteryEnough(int requiredAmount) {
+        if (batteryLevel >= requiredAmount) return true;
+        else return false;
+    }
+    public int consumeBattery(int amount){
+        batteryLevel -= amount;
+        return batteryLevel;
+    }
+    public int chargeBattery(int amount){
+        if(batteryLevel>=100){
+            status = "Active";
+            return batteryLevel;
+        }
+        else status = "Charging"; return batteryLevel += amount;
+    }
 }
