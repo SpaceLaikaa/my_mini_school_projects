0ff4b773f78899cddf51589b70a4ee6fe44fbe44
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-12-04 22:21:51 +0300

    Created Robot class with properties (modelName, batteryLevel, status), Implemented constructor, getters, setters, and display method

--- lab_projects/lab8/L8Assignment1/FactoryDemoL8A1.java
+++ lab_projects/lab8/L8Assignment1/FactoryDemoL8A1.java
@@ -1,5 +1,14 @@
 package lab_projects.lab8.L8Assignment1;
 
 public class FactoryDemoL8A1 {
+    public void main(String[] args) {
+        Robot Robo1 = new Robot("Nagasaki-15", 100,"Active");
+        Robot Robo2 = new Robot("Osaka-38", 100,"Active");
 
+        System.out.println("=====Robot 1=====");
+        Robo1.displayRobotInfo();
+        System.out.println("");
+        System.out.println("=====Robot 2=====");
+        Robo2.displayRobotInfo();
+    }
 }
--- lab_projects/lab8/L8Assignment1/Robot.java
+++ lab_projects/lab8/L8Assignment1/Robot.java
@@ -1,7 +1,28 @@
 package lab_projects.lab8.L8Assignment1;
 
+import java.util.concurrent.BlockingDeque;
+
 public class Robot {
     private String modelName;
     private int batteryLevel;
     private String status;
+
+    public Robot(String MN, int BL, String status){
+        this.modelName = MN;
+        this.batteryLevel = BL;
+        this.status = status;
+    }
+
+    public void displayRobotInfo(){
+        System.out.println("Model Name: " + modelName +"\nBattery Level: " + batteryLevel + "%\nStatus: " + status);
+    }
+
+    public String getModelName(){return modelName;}
+    public int getBatteryLevel(){return batteryLevel;}
+    public String getStatus(){return status;}
+
+    public void setModelName(String modelName){this.modelName = modelName;}
+    public void setBatteryLevel(int batteryLevel){this.batteryLevel = batteryLevel;}
+    public void setStatus(String status){this.status = status;}
+
 }
